6336e1ed7c71b0c0e4209072dcce1a4eeeca1ebb
nitishsharma2825 <sharnit2825@gmail.com>
2024-12-30 19:55:13 +0530

    hashtable: initialization

---
+++ data-structures/hashtable/src/hash_table.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char* k, const char* v)
+{
+    ht_item *i = (ht_item*)malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
+
+static void ht_del_item(ht_item* i)
+{
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+ht_hash_table* ht_new()
+{
+    ht_hash_table* ht = (ht_hash_table*)malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    return ht;
+}
+
+void ht_del_hash_table(ht_hash_table* ht)
+{
+    for (int i = 0; i < ht->size; i++)
+    {
+        ht_item* item = ht->items[i];
+        if (item != NULL)
+        {
+            free(item);
+        }
+    }
+    free(ht->items);
+    free(ht);
+}
+
